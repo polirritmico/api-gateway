a3ee8bfae5baa0cd70ffd81d8d5313bd18aa5623
polirritmico <ebray187@gmail.com>
2026-06-21 22:16:30 -0400

    feat: add requests logger

---
+++ src/main/java/cl/duoc/api_gateway/ApiGatewayLoggingFilter.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.api_gateway;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@Slf4j
+public class ApiGatewayLoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        log.info("Request received: [{}] '{}' ", request.getMethod(), request.getRequestURI());
+        filterChain.doFilter(request, response);
+    }
+}
