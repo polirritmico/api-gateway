b4879f50d4459045519e4d881ea1361970b6c7a2
polirritmico <ebray187@gmail.com>
2026-06-21 22:01:38 -0400

    feat(ApiGatewayApplication): add Eureka discovery client

--- src/main/java/cl/duoc/api_gateway/ApiGatewayApplication.java
+++ src/main/java/cl/duoc/api_gateway/ApiGatewayApplication.java
@@ -1,13 +1,20 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
 package cl.duoc.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
